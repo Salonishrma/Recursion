1e6ad8414fd20618e4dc7bd9ec9f71b4398f3889
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-02-12 19:59:55 +0530

    Create count.cpp

---
+++ count.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+using namespace std;
+void count(int n){
+    if(n==0){
+       return ;
+    }
+    count(n-1); 
+    cout<<n<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+     cout<<"power is equal to ";
+    count(n);
+    return 0;
+}
